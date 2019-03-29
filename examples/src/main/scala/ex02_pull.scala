b4d01f6f5db665452a4a766aa2987ea65a7bba06
Lukasz Byczynski <mail@lukaszbyczynski.com>
2019-03-29 23:50:01 +0100

    Pull example

---
+++ examples/src/main/scala/ex02_pull.scala
@@ -0,0 +1,36 @@
+import cats.Monoid
+import cats.implicits._
+import fs2._
+
+object ex02_pull extends App {
+
+  def sumOperator[F[_], A: Monoid](range: Int): Pipe[F, A, A] = { in =>
+
+    def go(stream: Stream[F, A], counter: Int, sum: A): Pull[F, A, Unit] = {
+      stream.pull.unconsLimit(100).flatMap {
+        case Some((chunk, tailStream)) =>
+          val newCounter = counter + chunk.size
+          if (newCounter < range)
+            go(tailStream, newCounter, chunk.foldLeft(sum)(_ |+| _))
+          else {
+            val n = newCounter - range
+            val outputSum = chunk.take(n).foldLeft(sum)(_ |+| _)
+
+            Pull.output1(outputSum) >> go(Stream.chunk(chunk.drop(n)) ++ tailStream, 0, Monoid[A].empty)
+          }
+
+        case None =>
+          Pull.output1(sum) >> Pull.done
+      }
+    }
+
+    go(in, 0, Monoid[A].empty).stream
+  }
+
+  Stream
+    .range(1, 100000)
+    .through(sumOperator(4))
+    .compile
+    .toList
+    .foreach(println)
+}
--- examples/src/main/scala/scalar/fs2/cc/ex02_pull.scala
+++
@@ -1,3 +0,0 @@
-package scalar.fs2.cc
-
-object ex02_pull extends App {}
