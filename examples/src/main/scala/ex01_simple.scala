4ca0c7eb26af2dded70841187f643e746120ef1c
Lukasz Byczynski <mail@lukaszbyczynski.com>
2019-04-04 00:16:01 +0200

    More examples

--- examples/src/main/scala/ex01_simple.scala
+++ examples/src/main/scala/ex01_simple.scala
@@ -1,7 +1,37 @@
+import scala.util.Random
+
 import fs2._
+import cats.effect.IO
 
 object ex01_simple extends App {
 
+  val list = Stream(1, 2, 3).compile.toList
+  println(list)
+
+  val range = Stream.eval(IO(Random.nextInt())).compile.lastOrError
+  println(range.unsafeRunSync())
+
+  val rangeRepeted = Stream.repeatEval(IO(Random.nextInt())).take(4).compile.toList
+  println(rangeRepeted.unsafeRunSync())
+
   val composed = Stream.range(1, 4) ++ Stream("a", "b", "c")
   println(composed.compile.toList)
+
+  val zipped = Stream("Chicago", "Berlin", "Warsaw")
+    .zip(Stream("USA", "Germany", "Poland"))
+    .map { case (city, country) => s"$city - $country" }
+    .compile
+    .toList
+  println(zipped)
+
+  val unfoldWitEvalMap = Stream
+    .unfold(1)(prev => if (prev == 5) None else Some((prev, prev + 1)))
+    .evalMap(i => IO(println(i)))
+    // .flatMap(i => Stream.eval(IO(println(i))))
+    .compile
+    .drain
+  unfoldWitEvalMap.unsafeRunSync()
+
+  val concat = (Stream.range(1, 4) ++ Stream(4, 5)).compile.toList
+  println(concat)
 }
